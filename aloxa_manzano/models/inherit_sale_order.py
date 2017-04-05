95bf6126e28c37a047c71d4748899f1fbfde1c2f
Dario Lodeiros <dario@aloxa.eu>
2017-04-05 12:10:30 +0200

    ADD report module

---
+++ aloxa_manzano/models/inherit_sale_order.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# --------------------------------------------------------------------------
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2012-Today Serpent Consulting Services PVT. LTD.
+#    (<http://www.serpentcs.com>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+# ---------------------------------------------------------------------------
+from openerp import models, fields, api, _
+
+
+class SaleOrder(models.Model):
+
+    _inherit = 'sale.order'
+
+    @api.onchange('amount_total')
+    def _pay_signal(self):
+	for order in self:
+		percent = self.env.user.company_id.default_signal
+		order.update({'payment_signal':order.amount_total * percent/100,})
+     
+
+    payment_signal = fields.Monetary(string='Signal', store=True)
+
+
